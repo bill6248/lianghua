42f3d6b3caa5e6a221f1fef435a6bcb449101f9e
wzw <49608785+bill6248@users.noreply.github.com>
2019-04-28 17:26:17 +0800

    Update factor.py

--- factor.py
+++ factor.py
@@ -30,15 +30,11 @@ t = datetime.datetime.strptime("2006-12-1", "%Y-%m-%d")
 t = t.date()
 
 # 分析
-
-
 interval = 60  # 周期长度
-
 n = 6  # 分析 n 个周期
 for i in range(n):
     # 每次分析延后 interval 天
     t = t + datetime.timedelta(days=interval)
-
     sl = get_index_stocks('000300.XSHG', t)
     # 检验周期 开始日期
     lt = t - datetime.timedelta(days=interval)
@@ -48,7 +44,6 @@ for i in range(n):
     # 获取过去nk天因子均值
     nk = 5
     for k in range(nk):
-
         q = query(
             valuation.code,
             valuation.market_cap.label("vol"),
@@ -73,6 +68,7 @@ for i in range(n):
 
     # 获取周期内涨幅
     ct = 6  # 最近ct天股票收盘均价
+    
     price = get_price(sl, end_date=t, count=ct)
     stock_close = price['close'].T
     stock_close.insert(len(stock_close.columns), 'mean', stock_close.mean(1))  # 计算均值并且插入
@@ -84,6 +80,7 @@ for i in range(n):
     # 数据清洗
     stock_close1 = stock_close1.dropna()
     stock_close = stock_close.dropna()
+    
     # 计算均值涨幅
     zf = (stock_close['mean'] - stock_close1['mean']) / stock_close1['mean']
 
@@ -94,22 +91,22 @@ for i in range(n):
     rl = []
     rl2 = []
     # 建立 结果df
-    # 分析 每个因子 的 相关性 放进rl rl2 临时结果存放
+    # 分析每个因子与涨幅的秩相关性，将结果放进rl rl2 ，临时结果存放
     for i in range(len(df.columns) - 1):
         factor = df.columns[i]
         coef, p = stats.spearmanr(df[factor], df['zf'])
         rl.append(coef)
         rl2.append(p)
 
-    # 插入新的分析结果进入结果dataframe
-
+    # 插入新的分析结果进入final结果dataframe
     rs1 = pd.DataFrame(columns=["vol", "pe", "roe", "pb", 'tr'], data=[rl])
     #插入最终rs数据
     finalres = finalres.append(rs1, ignore_index=True)
+    
+    # 插入新的统计检验结果进入res_test
     rsp1 = pd.DataFrame(columns=["vol", "pe", "roe", "pb", 'tr'], data=[rl2])
     #统计学检验
     res_test = res_test.append(rsp1, ignore_index=True)
 
 # 画出分布图
-
 finalres.plot(kind='kde')
